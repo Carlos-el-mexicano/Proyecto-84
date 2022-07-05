78c34450ad286e4b5e6d16124b6a9ca8ae68c513
Carlos-el-mexicano <100881116+Carlos-el-mexicano@users.noreply.github.com>
2022-07-05 13:55:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+	{
+		alphabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+		console.log("alphabet key");
+	}
+}
+
+function alphabetkey()
+{
+     	img_image="Alpkey.png";
+	add();
+}
